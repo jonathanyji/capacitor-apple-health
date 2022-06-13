a81549454be669826c503cf7775f6f5dae46c1ad
Jonathan Yeo <jonathan+1@inspiretek.io>
2022-06-13 11:36:31 +1000

    Initial commit

---
+++ android/src/main/java/com/inspire/plugins/io/Health.java
@@ -0,0 +1,11 @@
+package com.inspire.plugins.io;
+
+import android.util.Log;
+
+public class Health {
+
+    public String echo(String value) {
+        Log.i("Echo", value);
+        return value;
+    }
+}
